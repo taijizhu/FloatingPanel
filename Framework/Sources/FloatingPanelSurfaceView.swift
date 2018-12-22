a0087b527928bb38da780893950f2e2a9d78097f
Cao Yuchao <taijizhu@gmail.com>
2018-12-22 10:38:31 +0800

    quick fix

--- Framework/Sources/FloatingPanelSurfaceView.swift
+++ Framework/Sources/FloatingPanelSurfaceView.swift
@@ -5,29 +5,29 @@
 
 import UIKit
 
-class FloatingPanelSurfaceContentView: UIView {}
+@objc class FloatingPanelSurfaceContentView: UIView {}
 
 /// A view that presents a surface interface in a floating panel.
-public class FloatingPanelSurfaceView: UIView {
+@objc public class FloatingPanelSurfaceView: UIView {
 
     /// A GrabberHandleView object displayed at the top of the surface view.
     ///
     /// To use a custom grabber handle, hide this and then add the custom one
     /// to the surface view at appropirate coordinates.
-    public var grabberHandle: GrabberHandleView!
+    @objc public var grabberHandle: GrabberHandleView!
 
     /// The height of the grabber bar area
-    public static var topGrabberBarHeight: CGFloat {
+    @objc public static var topGrabberBarHeight: CGFloat {
         return Default.grabberTopPadding * 2 + GrabberHandleView.Default.height // 17.0
     }
 
     /// A UIView object that can have the surface view added to it.
-    public var contentView: UIView!
+    @objc public var contentView: UIView!
 
     private var color: UIColor? = .white { didSet { setNeedsLayout() } }
     private var bottomOverflow: CGFloat = 0.0 // Must not call setNeedsLayout()
 
-    public override var backgroundColor: UIColor? {
+    @objc public override var backgroundColor: UIColor? {
         get { return color }
         set { color = newValue }
     }
@@ -36,28 +36,28 @@ public class FloatingPanelSurfaceView: UIView {
     ///
     /// `self.contentView` is masked with the top rounded corners automatically on iOS 11 and later.
     /// On iOS 10, they are not automatically masked because of a UIVisualEffectView issue. See https://forums.developer.apple.com/thread/50854
-    public var cornerRadius: CGFloat = 0.0 { didSet { setNeedsLayout() } }
+    @objc public var cornerRadius: CGFloat = 0.0 { didSet { setNeedsLayout() } }
 
     /// A Boolean indicating whether the surface shadow is displayed.
-    public var shadowHidden: Bool = false  { didSet { setNeedsLayout() } }
+    @objc public var shadowHidden: Bool = false  { didSet { setNeedsLayout() } }
 
     /// The color of the surface shadow.
-    public var shadowColor: UIColor = .black  { didSet { setNeedsLayout() } }
+    @objc public var shadowColor: UIColor = .black  { didSet { setNeedsLayout() } }
 
     /// The offset (in points) of the surface shadow.
-    public var shadowOffset: CGSize = CGSize(width: 0.0, height: 1.0)  { didSet { setNeedsLayout() } }
+    @objc public var shadowOffset: CGSize = CGSize(width: 0.0, height: 1.0)  { didSet { setNeedsLayout() } }
 
     /// The opacity of the surface shadow.
-    public var shadowOpacity: Float = 0.2 { didSet { setNeedsLayout() } }
+    @objc public var shadowOpacity: Float = 0.2 { didSet { setNeedsLayout() } }
 
     /// The blur radius (in points) used to render the surface shadow.
-    public var shadowRadius: CGFloat = 3  { didSet { setNeedsLayout() } }
+    @objc public var shadowRadius: CGFloat = 3  { didSet { setNeedsLayout() } }
 
     /// The width of the surface border.
-    public var borderColor: UIColor?  { didSet { setNeedsLayout() } }
+    @objc public var borderColor: UIColor?  { didSet { setNeedsLayout() } }
 
     /// The color of the surface border.
-    public var borderWidth: CGFloat = 0.0  { didSet { setNeedsLayout() } }
+    @objc public var borderWidth: CGFloat = 0.0  { didSet { setNeedsLayout() } }
 
     private var backgroundLayer: CAShapeLayer!  { didSet { setNeedsLayout() } }
 
@@ -157,14 +157,14 @@ public class FloatingPanelSurfaceView: UIView {
         }
     }
 
-    func set(bottomOverflow: CGFloat) {
+    @objc func set(bottomOverflow: CGFloat) {
         self.bottomOverflow = bottomOverflow
         updateLayers()
         updateContentViewMask()
     }
 
 
-    func add(childView: UIView) {
+    @objc func add(childView: UIView) {
         contentView.addSubview(childView)
         childView.frame = contentView.bounds
         childView.translatesAutoresizingMaskIntoConstraints = false
